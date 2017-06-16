18c824c4677806207a9f3da46de4e2ae78f82761
Diego Augusto <daugustomoura@gmail.com>
2017-06-16 17:28:10 -0300

    aula 02

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        rb.velocity = new Vector3(speed, 0, 0);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SwitchDiretion();
+        }
+    }
+
+    private void SwitchDiretion()
+    {
+        if (rb.velocity.z > 0)
+        {
+            rb.velocity = new Vector3(speed, 0, 0);
+        }
+        else if (rb.velocity.x > 0)
+        {
+            rb.velocity = new Vector3(0, 0, speed);
+        }
+    }
+}
