2134c5852f8d1e75975c6882207879236f049cff
Diego Augusto <daugustomoura@gmail.com>
2017-06-23 20:37:02 -0300

    aula 07:
    	- Função de criar plataformas na posição X ou Y

---
+++ Assets/Scripts/PlataformSpawnerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PlataformSpawnerController : MonoBehaviour
+{
+
+    public GameObject plataform;
+    private Vector3 lastpos;
+    private float plataformSize;
+
+    void Start()
+    {
+        lastpos = plataform.transform.position;
+        plataformSize = plataform.transform.localScale.x;
+
+        for (int i = 0; i < 5; i++)
+        {
+            SpawnX();
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            SpawnZ();
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void SpawnX()
+    {
+        Vector3 pos = lastpos;
+        pos.x += plataformSize;
+        Instantiate(plataform, pos, Quaternion.identity);
+        lastpos = pos;
+    }
+
+    public void SpawnZ()
+    {
+        Vector3 pos = lastpos;
+        pos.z += plataformSize;
+        Instantiate(plataform, pos, Quaternion.identity);
+        lastpos = pos;
+    }
+}
