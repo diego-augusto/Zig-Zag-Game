1182c51eae08c34418c941055c44637b04a07540
Diego Augusto <daugustomoura@gmail.com>
2017-06-23 20:10:28 -0300

    aula 06: derrubando plataformas

---
+++ Assets/Scripts/PlataformTriggerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlataformTriggerController : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Ball"))
+        {
+            Invoke("Fall", 0.5f);
+        }
+    }
+
+    private void Fall()
+    {
+        GetComponentInParent<Rigidbody>().useGravity = true;
+        Destroy(transform.parent.gameObject, 2.0f);
+    }
+}
